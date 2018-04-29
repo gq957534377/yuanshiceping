1ca6bba6fe37eb57a489afb4c3dd7fa47b73b50b
guoqing <957534377@qq.com>
2018-04-29 13:33:29 +0800

    初始化数据库完毕

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name')->nullable();
+            $table->string('tel')->nullable();
+            $table->string('password')->nullable();
+            $table->string('open_id')->nullable();
+            $table->string('head_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
