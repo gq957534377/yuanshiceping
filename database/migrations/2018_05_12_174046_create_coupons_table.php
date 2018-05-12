a057d9963432ac3adf7125134045876ee2716b8f
guoqing <957534377@qq.com>
2018-05-12 21:00:19 +0800

    创建优惠券表完成

---
+++ database/migrations/2018_05_12_174046_create_coupons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->char('id',32)->primary()->comment('优惠券码');
+            $table->integer('user_id')->nullable()->comment('生成人');
+            $table->integer('min')->nullable()->comment('优惠最小额度');
+            $table->integer('max')->nullable()->comment('优惠最大额度');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
---
+++ database/migrations/2018_05_12_205244_create_coupons_rel_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCouponsRelUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons_rel_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('id', 32)->comment('优惠券码');
+            $table->integer('user_id')->nullable()->comment('生成人');
+            $table->integer('price')->nullable()->comment('优惠金额');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons_rel_users');
+    }
+}
