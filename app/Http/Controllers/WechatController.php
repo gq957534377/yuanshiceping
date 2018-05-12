92ed793376e6ad4a7c8adef46d8bfd13328d1d37
guoqing <957534377@qq.com>
2018-05-12 11:30:27 +0800

    hh

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -81,6 +81,6 @@ class WechatController extends Controller
         $response = $app->server->serve();
 
 // 将响应输出
-        $response->send(); // Laravel 里请使用：return $response;
+        return $response->send(); // Laravel 里请使用：return $response;
     }
 }
