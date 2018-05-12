61c74233414238d8ea90b15ab7e51160e3c4ff14
guoqing <957534377@qq.com>
2018-05-12 09:31:19 +0800

    hh

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -32,6 +32,7 @@ class WechatController extends Controller
 
     public function index()
     {
+        return "11111";
         return $this->app->server->serve();
         //消息自动回复
         $this->app->server->setMessageHandler(function ($message){
