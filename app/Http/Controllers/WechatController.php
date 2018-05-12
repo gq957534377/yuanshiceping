9a60bcbfd516913a1e8a1d3aef272a72c06f6c6b
guoqing <957534377@qq.com>
2018-05-12 10:54:15 +0800

    hh

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -34,7 +34,7 @@ class WechatController extends Controller
     {
         \Log::info(111111);
         //消息自动回复
-        $this->app->server->push(function ($message){
+//        $this->app->server->push(function ($message){
 //            switch ($message['MsgType']) {
 //                case 'event':
 //                    switch ($message->Event) {
@@ -59,8 +59,32 @@ class WechatController extends Controller
 //                default:
 //                    return '该功能正在玩命开发中。。。';
 //            }
-            return "sjhdghsgdh"
-;        });
+//            return "sjhdghsgdh"
+//;        });
+ddd(config('wechat'));
+        $options = [
+            'app_id'    => 'wx3cf0f39249eb0exxx',
+            'secret'    => 'f1c242f4f28f735d4687abb469072xxx',
+            'token'     => 'easywechat',
+            'log' => [
+                'level' => 'debug',
+                'file'  => '/tmp/easywechat.log',
+            ],
+            // ...
+        ];
+
+        $app = Factory::officialAccount($options);
+
+        $server = $app->server;
+        $user = $app->user;
+
+        $server->push(function($message) use ($user) {
+            $fromUser = $user->get($message['FromUserName']);
+
+            return "{$fromUser->nickname} 您好！欢迎关注 overtrue!";
+        });
+
+        $server->serve()->send();
 
         return $this->app->server->serve()->send();
     }
