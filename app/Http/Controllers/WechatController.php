74f845f49f66633919ed47cbefd9f8ae81088a6d
guoqing <957534377@qq.com>
2018-05-14 16:54:39 +0800

    hh

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -88,7 +88,10 @@ class WechatController extends Controller
                                     // 给邀请人积分加一，并且推送消息给邀请人 todo 后期改成异步队列
                                     $newUser['inviter_id'] = User::where('weChat_id', $message['EventKey'])->first()->id??null;
                                     $count = User::where('inviter_id', $newUser['inviter_id'])->count();
-                                    event(new MessageRemind($newUser['inviter_id'], 'Hj3J34GjEweQ6aFSmuIZ8GbACGYK7-skjiEam_arUrU', ['name' => $message['FromUserName'], 'num' => $count]));
+                                    event(new MessageRemind($newUser['inviter_id'], 'Hj3J34GjEweQ6aFSmuIZ8GbACGYK7-skjiEam_arUrU', [
+                                        'name' => $app->user->get($message['FromUserName'])['nickname'],
+                                        'num' => $count
+                                    ]));
                                 }
 
                                 // todo 如果够了指标，发送通知
