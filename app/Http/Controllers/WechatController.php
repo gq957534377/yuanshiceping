bda085cc2d45bbb26671c3be1032b77ee3c8c2a9
guoqing <957534377@qq.com>
2018-05-14 17:43:25 +0800

    hh

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -98,7 +98,7 @@ class WechatController extends Controller
                                 // todo 如果够了指标，发送通知
 
                                 // 根据用户open_id生成二维码并且返回
-                                $result = $app->qrcode->forever(1);
+                                $result = $app->qrcode->forever($weChat['openid']);
                                 Log::error($message['FromUserName']);
                                 $url = $app->qrcode->url($result['ticket']);
                                 $newUser['ticket'] = $result['ticket'];
