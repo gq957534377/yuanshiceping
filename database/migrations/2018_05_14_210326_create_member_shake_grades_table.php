169efe5d9baf52a215faf050ed98c928dfa05f8e
zhaochang <80330582@qq.com>
2018-05-14 21:39:33 +0800

    生成迁移文件

---
+++ database/migrations/2018_05_14_210326_create_member_shake_grades_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMemberShakeGradesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('member_shake_grades', function(Blueprint $table)
+		{
+			$table->integer('member_id')->unsigned()->index('member_id');
+			$table->integer('shake_id')->unsigned()->index('shake_id');
+			$table->decimal('grade', 4, 1)->unsigned()->default(0.0);
+			$table->primary(['member_id','shake_id']);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('member_shake_grades');
+	}
+
+}
