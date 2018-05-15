aa67e0255c604683d9be1169c1009e26c067837e
guoqing <957534377@qq.com>
2018-05-15 09:15:01 +0800

    hh

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -18,7 +18,7 @@ class LoginController extends Controller
      */
     public function login(LoginRequest $request)
     {
-        dd($request->all());
+        dd($request->all(),$request->open_id);
         // 判断当前用户open_id是否已经存在
         $user = User::where('open_id', $request->open_id)->first();
 
