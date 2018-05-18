90784e300c75d2fbb4553fb4c0f524d7a2d78403
guoqing <957534377@qq.com>
2018-05-18 16:42:42 +0800

    hh

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -12,7 +12,7 @@ class Comment extends Model
 
     public function user()
     {
-        return $this->hasOne(User::class,'id','user_id');
+        return $this->hasOne(User::class, 'id', 'user_id');
     }
 
     public static function getItems(self $item)
@@ -23,8 +23,9 @@ class Comment extends Model
             'parent_id' => $item->parent_id,
             'user_id' => $item->user_id,
             'title' => $item->title,
-            'content' => $item->content,
-            'user_head'=>$item->user->head_url
+            'content' => str_limit($item->content, 66),
+            'user_head' => $item->user->head_url,
+            'updated_at' => $item->updated_at,
         ];
     }
 }
