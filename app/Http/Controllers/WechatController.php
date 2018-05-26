1bc97acb96caf5566b57456df0e91e0c0b1b5746
guoqing <957534377@qq.com>
2018-05-26 11:23:27 +0800

    hh

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -60,6 +60,50 @@ class WechatController extends Controller
     public function index()
     {
         $app = $this->app;
+        $buttons = [
+            [
+                "name" => "福利领取",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "免费测评",
+                        "url" => "http://www.soso.com/"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "微课",
+                        "url" => "http://v.qq.com/"
+                    ],
+                ]
+            ],
+            [
+                "type" => "click",
+                "name" => "测评大厅",
+                "key" => "V1001_TODAY_MUSIC"
+            ],
+            [
+                "name" => "关于我们",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "企业简介",
+                        "url" => "http://www.soso.com/"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "服务案例",
+                        "url" => "http://v.qq.com/"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "联系我们",
+                        "url" => "http://v.qq.com/"
+                    ],
+                ]
+            ],
+        ];
+        $app->menu->create($buttons);
+
         $app->server->push(function ($message) use ($app) {
             switch ($message['MsgType']??null) {
                 case 'event':
@@ -115,7 +159,7 @@ class WechatController extends Controller
                                 return $url;
 
 
-                                $bottomImg = $this->makeImg($newUser['head_url'],$newUser['name'],$url);
+                                $bottomImg = $this->makeImg($newUser['head_url'], $newUser['name'], $url);
 
                                 $upload = $this->uploadImage($bottomImg, $user->ticket);
                                 return new Image($upload['media_id']);
@@ -182,37 +226,37 @@ class WechatController extends Controller
      * @param $qrcode
      * @return string
      */
-    public function makeImg($headImg,$name,$qrcode)
+    public function makeImg($headImg, $name, $qrcode)
     {
-        $tx = $this->getImage($headImg, public_path('uploads/wechatHeadImg/'),time().mt_rand(1,9999).'.jpeg',1);
+        $tx = $this->getImage($headImg, public_path('uploads/wechatHeadImg/'), time() . mt_rand(1, 9999) . '.jpeg', 1);
         $tx = imagecreatefromjpeg($this->headImg($tx['save_path']));
 
         // 底板图片
-        $bottom = imagecreatefromjpeg (public_path('uploads/wechatInvitationImage/bottomJscp.jpeg'));
+        $bottom = imagecreatefromjpeg(public_path('uploads/wechatInvitationImage/bottomJscp.jpeg'));
 
-        $font = public_path('font').'/PingFangRegular.ttf';
+        $font = public_path('font') . '/PingFangRegular.ttf';
         //微信头像
-        imagecopymerge($bottom, $tx,30 , 30, 0, 0, 120, 120, 100);
+        imagecopymerge($bottom, $tx, 30, 30, 0, 0, 120, 120, 100);
         //微信名
-        imagefttext($bottom, 14, 0, 170, 30, imagecolorallocate($bottom, 99,75,0), $font, $name);
+        imagefttext($bottom, 14, 0, 170, 30, imagecolorallocate($bottom, 99, 75, 0), $font, $name);
         // 二维码
-        $qr = $this -> $this->getImage($qrcode,public_path('uploads/qrcode/'),time().mt_rand(1,9999).'.jpeg',1);
-        $wh  = getimagesize($qr['save_path']);
-        $w   = $wh[0];
-        $h   = $wh[1];
-        $w   = min($w, $h);
-        $h   = $w;
-        $new=imagecreatetruecolor(160, 160);
+        $qr = $this->$this->getImage($qrcode, public_path('uploads/qrcode/'), time() . mt_rand(1, 9999) . '.jpeg', 1);
+        $wh = getimagesize($qr['save_path']);
+        $w = $wh[0];
+        $h = $wh[1];
+        $w = min($w, $h);
+        $h = $w;
+        $new = imagecreatetruecolor(160, 160);
         //copy部分图像并调整
-        imagecopyresized($new, $qr,0, 0,0, 0,160, 160, $w, $h);
+        imagecopyresized($new, $qr, 0, 0, 0, 0, 160, 160, $w, $h);
 
         // 合并图像
-        imagecopymerge($bottom, $new,500 , 1160, 0, 0, 160, 160, 100);
+        imagecopymerge($bottom, $new, 500, 1160, 0, 0, 160, 160, 100);
 
 
-        $fileName = public_path('uploads/wechatInvitationImage/').time().mt_rand(1,9999).'.jpeg';
+        $fileName = public_path('uploads/wechatInvitationImage/') . time() . mt_rand(1, 9999) . '.jpeg';
         header('Content-type:image/jpeg');
-        imagejpeg($bottom,$fileName,100);
+        imagejpeg($bottom, $fileName, 100);
         imagedestroy($bottom);
         return $fileName;
     }
@@ -222,50 +266,50 @@ class WechatController extends Controller
      * 参数：文件url,保存文件目录,保存文件名称，使用的下载方式
      * 当保存文件名称为空时则使用远程文件原来的名称
      */
-    public function getImage($url,$save_dir='',$filename='',$type=0)
+    public function getImage($url, $save_dir = '', $filename = '', $type = 0)
     {
-        if(trim($url)==''){
-            return array('file_name'=>'','save_path'=>'','error'=>1);
+        if (trim($url) == '') {
+            return array('file_name' => '', 'save_path' => '', 'error' => 1);
         }
-        if(trim($save_dir)==''){
-            $save_dir='./';
+        if (trim($save_dir) == '') {
+            $save_dir = './';
         }
-        if(trim($filename)==''){//保存文件名
-            $ext=strrchr($url,'.');
-            if($ext!='.gif'&&$ext!='.jpg'){
-                return array('file_name'=>'','save_path'=>'','error'=>3);
+        if (trim($filename) == '') {//保存文件名
+            $ext = strrchr($url, '.');
+            if ($ext != '.gif' && $ext != '.jpg') {
+                return array('file_name' => '', 'save_path' => '', 'error' => 3);
             }
-            $filename=time().$ext;
+            $filename = time() . $ext;
         }
-        if(0!==strrpos($save_dir,'/')){
-            $save_dir.='/';
+        if (0 !== strrpos($save_dir, '/')) {
+            $save_dir .= '/';
         }
         //创建保存目录
-        if(!file_exists($save_dir)&&!mkdir($save_dir,0777,true)){
-            return array('file_name'=>'','save_path'=>'','error'=>5);
+        if (!file_exists($save_dir) && !mkdir($save_dir, 0777, true)) {
+            return array('file_name' => '', 'save_path' => '', 'error' => 5);
         }
         //获取远程文件所采用的方法
-        if($type){
-            $ch=curl_init();
-            $timeout=5;
-            curl_setopt($ch,CURLOPT_URL,$url);
-            curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-            curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
-            $img=curl_exec($ch);
+        if ($type) {
+            $ch = curl_init();
+            $timeout = 5;
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+            $img = curl_exec($ch);
             curl_close($ch);
-        }else{
+        } else {
             ob_start();
             readfile($url);
-            $img=ob_get_contents();
+            $img = ob_get_contents();
             ob_end_clean();
         }
         //$size=strlen($img);
         //文件大小
-        $fp2=@fopen($save_dir.$filename,'a');
-        fwrite($fp2,$img);
+        $fp2 = @fopen($save_dir . $filename, 'a');
+        fwrite($fp2, $img);
         fclose($fp2);
-        unset($img,$url);
-        return array('file_name'=>$filename,'save_path'=>$save_dir.$filename,'error'=>0);
+        unset($img, $url);
+        return array('file_name' => $filename, 'save_path' => $save_dir . $filename, 'error' => 0);
     }
 
     /**
@@ -276,7 +320,7 @@ class WechatController extends Controller
      */
     public function headImg($url)
     {
-        $ext     = pathinfo($url);
+        $ext = pathinfo($url);
 
         $src_img = null;
         switch ($ext['extension']) {
@@ -287,11 +331,11 @@ class WechatController extends Controller
                 $src_img = imagecreatefrompng($url);
                 break;
         }
-        $wh  = getimagesize($url);
-        $w   = $wh[0];
-        $h   = $wh[1];
-        $w   = min($w, $h);
-        $h   = $w;
+        $wh = getimagesize($url);
+        $w = $wh[0];
+        $h = $wh[1];
+        $w = min($w, $h);
+        $h = $w;
         $img = imagecreatetruecolor($w, $h);
 
         //这一句一定要有
@@ -299,7 +343,7 @@ class WechatController extends Controller
         //拾取一个完全透明的颜色,最后一个参数127为全透明
         $bg = imagecolorallocatealpha($img, 255, 255, 255, 116);
         imagefill($img, 0, 0, $bg);
-        $r   = $w / 2; //圆半径
+        $r = $w / 2; //圆半径
         for ($x = 0; $x < $w; $x++) {
             for ($y = 0; $y < $h; $y++) {
                 $rgbColor = imagecolorat($src_img, $x, $y);
@@ -308,15 +352,15 @@ class WechatController extends Controller
                 }
             }
         }
-        $n_w=120;
-        $n_h=120;
-        $new=imagecreatetruecolor($n_w, $n_h);
+        $n_w = 120;
+        $n_h = 120;
+        $new = imagecreatetruecolor($n_w, $n_h);
         //copy部分图像并调整
-        imagecopyresized($new, $img,0, 0,0, 0,$n_w, $n_h, $w, $h);
+        imagecopyresized($new, $img, 0, 0, 0, 0, $n_w, $n_h, $w, $h);
 
-        $fileName = public_path('uploads/wechatHeadImg/').md5($url).'.jpeg';
+        $fileName = public_path('uploads/wechatHeadImg/') . md5($url) . '.jpeg';
         header("content-type:image/jpeg");
-        imagejpeg($new,$fileName);
+        imagejpeg($new, $fileName);
         imagedestroy($new);
         return $fileName;
     }
