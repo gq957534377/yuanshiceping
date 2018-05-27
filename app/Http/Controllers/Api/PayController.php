45458252e18ae1e261f6f977b758f7822ed1bf1e
guoqing <957534377@qq.com>
2018-05-27 20:09:49 +0800

    hh

--- app/Http/Controllers/Api/PayController.php
+++ app/Http/Controllers/Api/PayController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use EasyWeChat\Factory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -11,6 +12,15 @@ use function EasyWeChat\Kernel\Support\generate_sign;
 
 class PayController extends Controller
 {
+    private $app = null;
+
+    public function __construct()
+    {
+        $options = config('wechat.payment.default');
+
+        $this->app = Factory::officialAccount($options);
+    }
+
     /**
      * 测评支付微信统一下单
      *
@@ -59,6 +69,19 @@ class PayController extends Controller
     public function createOrder(Request $request)
     {
         $data = $request -> all();
+//        "return_code": "SUCCESS",
+//    "return_msg": "OK",
+//    "appid": "wx2421b1c4390ec4sb",
+//    "mch_id": "10000100",
+//    "nonce_str": "IITRi8Iabbblz1J",
+//    "openid": "oUpF8uMuAJO_M2pxb1Q9zNjWeSs6o",
+//    "sign": "7921E432F65EB8ED0CE9755F0E86D72F2",
+//    "result_code": "SUCCESS",
+//    "prepay_id": "wx201411102639507cbf6ffd8b0779950874",
+//    "trade_type": "JSAPI"
+
+        $this->app->order->queryByTransactionId("微信订单号（transaction_id）");
+
     }
 
 }
