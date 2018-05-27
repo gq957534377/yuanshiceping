19ea7bd568a5ece4e86b5f6eafbbb102df01ccc5
jing-yue <xyyspace@163.com>
2018-05-27 20:10:38 +0800

    bug

--- app/Admin/Controllers/CommentsController.php
+++ app/Admin/Controllers/CommentsController.php
@@ -10,7 +10,7 @@ use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Content;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
-use App\user;
+use App\User;
 use App\Models\Good;
 
 class CommentsController extends Controller
