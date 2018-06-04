68d4f6aacaf62680f9367f1d10807369c11568e0
guoqing <957534377@qq.com>
2018-06-04 14:34:37 +0800

    修改暂无数据

--- app/Http/Controllers/Api/GiftsController.php
+++ app/Http/Controllers/Api/GiftsController.php
@@ -24,7 +24,7 @@ class GiftsController extends Controller
         $user = Auth::guard('api')->user();
 
         if (empty($user->sendOrders->count())) {
-            return $this->sendResponse(['data'=>[]], '获取我送出的礼物完成');
+            return $this->sendResponse(['data' => []], '获取我送出的礼物完成');
         }
 
         $gifts = Gift::where('send_user', $user->id)->paginate($request->per_page??10);
@@ -32,7 +32,7 @@ class GiftsController extends Controller
             $order = Order::find($gift->order_id);
             $order->send_time = $gift->created_at;
             $order->title = $order->goods->goods_name??'';
-            $order->banner = url($order->goods->goods_image??'');
+            $order->banner = url('uploads/' . $order->goods->goods_image??'');
             $user = User::find($gift->receive_user);
             if (!empty($user)) {
                 $user->receive_time = $gift->updated_at->toDateTimeString();
@@ -61,7 +61,7 @@ class GiftsController extends Controller
     {
         $user = Auth::guard('api')->user();
         if (empty($user->receiveOrders->count())) {
-            return $this->sendResponse(['data'=>[]], '获取我收到的礼物完成');
+            return $this->sendResponse(['data' => []], '获取我收到的礼物完成');
         }
 
         $gifts = Gift::where('receive_user', $user->id)->paginate($request->per_page??10);
@@ -69,7 +69,7 @@ class GiftsController extends Controller
             $order = Order::find($gift->order_id);
             $order->send_time = $gift->created_at;
             $order->receive_time = $gift->updated_at;
-            $order->banner = url($order->goods->goods_image??'');
+            $order->banner = url('uploads/' . $order->goods->goods_image??'');
 
             $user = User::find($gift->send_user);
             if (!empty($user)) {
