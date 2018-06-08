7076637a92bb6934a0c7280cc6a14792edb320ad
zhaochang <80330582@qq.com>
2018-06-08 17:16:54 +0800

    才干得分显示，增加一个字段

--- resources/views/evaluation/grade-detail.blade.php
+++ resources/views/evaluation/grade-detail.blade.php
@@ -24,6 +24,7 @@
         <th>分数</th>
         <th>排名</th>
         <th>权重</th>
+        <th>对应相应风格加权重</th>
     </tr>
     @foreach($ability_grades as $grade)
         <tr>
@@ -31,6 +32,7 @@
             <td>{{ $grade['grade'] }}</td>
             <td>{{ $grade['rank'] }}</td>
             <td>{{ $grade['weight'] }}</td>
+            <td>{{ $grade['personality_type_weight'] }}</td>
 
         </tr>
     @endforeach
