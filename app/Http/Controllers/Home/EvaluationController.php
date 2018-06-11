12638a50f21582509cc6417d882c5ded71495873
jing-yue <xyyspace@163.com>
2018-06-11 12:12:50 +0800

    删除多余代码

--- app/Http/Controllers/Home/EvaluationController.php
+++ app/Http/Controllers/Home/EvaluationController.php
@@ -27,32 +27,6 @@ class EvaluationController extends Controller
         return view('evaluation.index');
     }
 
-    public function evaluate(Request $request)
-    {
-        $member = ['id'=>7];
-        $post = $request->post();
-
-
-        $answers = [];
-        $answers = array_merge($answers, Answer::makeAnswers($post['cat_a'],'cat_a', $member['id']));
-        $answers = array_merge($answers, Answer::makeAnswers($post['cat_b'],'cat_b', $member['id']));
-        $answers = array_merge($answers, Answer::makeAnswers($post['cat_c'],'cat_c', $member['id']));
-        if (!empty($answers)) {
-
-            Answer::insert($answers);
-        }
-
-        Answer::gradeCatA($member['id']); //计算兴趣
-        Answer::gradeCatB($member['id']); //才干 能力 得分
-        Answer::gradeCatC($member['id']); // 性格得分
-        Answer::gradeQuality($member['id']); //素质模型
-        Answer::gradePotential($member['id']); //计算潜能
-        Answer::gradeShake($member['id']); //计算型格
-        Answer::gradeMajor($member['id']); //计算专业
-
-        return ($post);
-    }
-
     public function reports()
     {
         $data = [];
