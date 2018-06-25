63b9c18aeabbd09a99396d51bdc08a58a932fce1
jing-yue <xyyspace@163.com>
2018-06-25 14:38:24 +0800

    增加查看报告按钮

--- resources/views/evaluation/reports.blade.php
+++ resources/views/evaluation/reports.blade.php
@@ -1,5 +1,9 @@
 <ul>
     @foreach($reports as $report)
-    <li><a href="{{ url('/grade-detail/'.$report->member_id.'/'.$report->order_number) }}">{{ $report->user_name }}-的报告</a>-{{ $report->created_at > 0 ? date('Y-m-d H:i:s', $report->created_at) : '' }}</li>
+    <li>
+        <a href="{{ url('/grade-detail/'.$report->member_id.'/'.$report->order_number) }}">{{ $report->user_name }}-的报告分数</a>
+        -{{ $report->created_at > 0 ? date('Y-m-d H:i:s', $report->created_at) : '' }}
+        <a href="{{ url('/report/'.$report->member_id.'?order_number='.$report->order_number) }}" target="_blank">{{ $report->user_name }}-的报告</a>
+    </li>
     @endforeach
 </ul>
